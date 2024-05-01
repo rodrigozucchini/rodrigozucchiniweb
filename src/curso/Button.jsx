70d00c6b4dc036947d538c659491df5b378da7cd
unknown <rodrigozucchini1997@gmail.com>
2024-05-01 00:47:21 -0300

    cambios en las imagenes

--- src/curso/Button.jsx
+++ src/curso/Button.jsx
@@ -4,7 +4,7 @@ import './Button.css';
 const Button = () => {
   return (
     <div className="button-div-call">
-        <a className="button-call" href="https://wa.link/yaw0tq" target="_blank">
+        <a className="button-call" href="https://wa.link/jn6les" target="_blank">
         ¡Quiero unirme al Programa ahora!
         </a>
     </div>
--- src/curso/IntroImg.jsx
+++ src/curso/IntroImg.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './IntroImg.css';
 import IntroImagen from '../assets/curso.png';
+import IntroImagen2 from '../assets/curso2.png';
 
 const IntroImg = () => {
   return (
     <div className='flyer'>
         <img className='flyer-img' src={IntroImagen} alt="mockup" />
+        <img className='flyer-img2' src={IntroImagen2} alt="mockup" />
     </div>
   )
 }
